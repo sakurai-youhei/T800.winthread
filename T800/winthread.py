170ef061ea33de0a5e90808fa9e1cf8635752ca3
Youhei Sakurai <sakurai.youhei@gmail.com>
2017-05-17 13:20:42 +0900

    Fix issue on Py3.5

--- T800/winthread.py
+++ T800/winthread.py
@@ -104,11 +104,11 @@ class TerminatableThread(Thread):
                 # Updating table recording all active threads.
                 del _active[self.ident]
 
-                # Masquerading as stopped
-                if hasattr(self, "_stop"):  # Py3.3
-                    self._stop()
-                elif hasattr(self, "_is_stopped"):  # Py3.6
+                # Masquerading as stopped (Ordered from modern to ancient ways)
+                if hasattr(self, "_is_stopped"):  # Py3.6
                     self._is_stopped = True
                     self._tstate_lock.release()
+                elif hasattr(self, "_stop"):  # Py3.3
+                    self._stop()
                 elif hasattr(self, "_Thread__stop"):  # Py2.7
                     self._Thread__stop()
