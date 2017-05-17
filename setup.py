bc45b122c2f4dce4edab076b1bed07753c2d2115
Youhei Sakurai <sakurai.youhei@gmail.com>
2017-05-17 11:50:16 +0900

    Drop 2.6

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
         License :: OSI Approved :: MIT License
         Development Status :: 4 - Beta
         Environment :: Win32 (MS Windows)
-        Programming Language :: Python :: 2.6
         Programming Language :: Python :: 2.7
         Programming Language :: Python :: 3.3
         Programming Language :: Python :: 3.4
